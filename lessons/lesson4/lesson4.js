4c16854fe9626c1785206957a3c57d75312bb65d
Denys <leldenis8@gmail.com>
2025-03-01 14:06:52 +0200

    learning type and methods

---
+++ lessons/lesson4/lesson4.js
@@ -0,0 +1,23 @@
+let message = "hello, world";
+console.log(message);
+{
+let message = "hello, den"; 
+console.log(message);
+}
+console.log (message);
+
+
+let counter = 0;
+let a = counter++;
+console.log(counter);
+console.log(a);
+
+
+
+// якщо хоч в одному є тру буде тру
+console.log(true || true);
+console.log(false || true);
+console.log(true || false);
+console.log(false || false);
+
+
