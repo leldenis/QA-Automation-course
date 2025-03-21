81dd1d94d23f6023590bf5cd0616e6ee750cf0a4
Denys <leldenis8@gmail.com>
2025-03-21 20:54:11 +0200

    to be continued...

---
+++ lessons/lessons_js/lesson10.js
@@ -0,0 +1,36 @@
+// class Calculator {
+//     add (a,b) {
+//         return a + b;
+//     }
+// }
+
+// const calculator = new Calculator();
+// const calculator2 = new Calculator();
+
+// console.log(calculator.add (3,5));
+// console.log(calculator2.add (2,6));
+
+
+// class Person {
+
+//     #counter = 0;
+
+//     constructor3 (age, name){
+//         this.name = name;
+//         this.age = age;
+//         this.#counter++;
+//     }
+//     GetAge () {
+//         return this.#counter;
+//     }
+// }
+
+// let person1 = new Person ('John, 30');
+// console.log(person1.GetAge());
+
+
+// const animalPrototype = {
+//     speak() {
+//         console.log(`${this.name} make a noise.`);
+//     }
+// }
